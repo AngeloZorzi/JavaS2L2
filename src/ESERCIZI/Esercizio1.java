fe09693ee6e99eebfa2e13b3d971aca9af2a9eee
AngeloZorzi <astro.drg666@gmail.com>
2025-05-06 15:26:37 +0200

    update

--- src/ESERCIZI/Esercizio1.java
+++ src/ESERCIZI/Esercizio1.java
@@ -1,8 +1,40 @@
 package ESERCIZI;
 
+import java.util.Scanner;
+
 public class Esercizio1 {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Scrivi una frase");
+        String testo = scanner.nextLine();
 
+        boolean risultato = pariDispari(testo);
+        System.out.println(risultato);
+
+
+        System.out.println("Dammi un anno, ti dirò se è bisestile:");
+        int anno = scanner.nextInt();
+        boolean isIt = annoBisestile(anno);
+        System.out.println("L'anno è bisestile? " + isIt);
+
+    }
+
+    public static boolean pariDispari(String testo){
+        if(testo.length()%2==0){
+            return false;
+        }else{
+            return true;
+        }
+    };
+
+    public static boolean annoBisestile(int anno) {
+        if (anno % 4 != 0) {
+            return false;
+        } else if (anno % 100 != 0) {
+            return true;
+        } else {
+            return anno % 400 == 0;
+        }
     }
 }
--- src/ESERCIZI/Esercizio2.java
+++ src/ESERCIZI/Esercizio2.java
@@ -2,6 +2,6 @@ package ESERCIZI;
 
 public class Esercizio2 {
     public static void main(String[] args) {
-        
+
     }
 }
