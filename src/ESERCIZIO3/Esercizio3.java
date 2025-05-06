95dd27b61790aef3f2550236e35265b312f41012
AngeloZorzi <astro.drg666@gmail.com>
2025-05-06 15:53:51 +0200

    update

--- src/ESERCIZIO3/Esercizio3.java
+++ src/ESERCIZIO3/Esercizio3.java
@@ -1,7 +1,28 @@
 package ESERCIZIO3;
 
+import java.util.Scanner;
+
 public class Esercizio3 {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        System.out.println("Inserisci una stringa (digita ':q' per uscire):");
+
+        while (true) {
+            input = scanner.nextLine();
+            if (input.equals(":q")) {
+                System.out.println("Programma terminato.");
+                break;
+            }
+            for (int i = 0; i < input.length(); i++) {
+                System.out.print(input.charAt(i));
+                if (i != input.length() - 1) {
+                    System.out.print(",");
+                }
+            }
+            System.out.println();
+        }
     }
 }
--- src/ESERCIZIO4/Esercizio4.java
+++ src/ESERCIZIO4/Esercizio4.java
@@ -1,7 +1,17 @@
 package ESERCIZIO4;
 
+import java.util.Scanner;
+
 public class Esercizio4 {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Inserisci un numero intero: ");
+        int numero = scanner.nextInt();
+
+        System.out.println("Conto alla rovescia:");
+        for (int i = numero; i >= 0; i--) {
+            System.out.println(i);
+        }
+
     }
 }
